02e8f6248b4de4139aa0fd35b3c4c903b056f528
amit.luthra1992@gmail.com <amit.luthra1992@gmail.com>
2022-07-01 10:35:04 +0530

    day 2

--- src/test/java/day1/RAday1.java
+++ src/test/java/day1/RAday1.java
@@ -1,8 +1,13 @@
 package day1;
 
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
+import bsh.org.objectweb.asm.Type;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Headers;
+
 import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 
@@ -15,7 +20,6 @@ public class RAday1 {
 		System.out.println(obj.asString());
 		System.out.println(obj.statusCode());
 		System.out.println(obj.headers());
-		
 	}
 
 	@Test(enabled = false)
@@ -25,7 +29,7 @@ public class RAday1 {
 		System.out.println(obj.asString());
 	}
 	
-	@Test(enabled = true)
+	@Test(enabled = false)
 	public void testcase3()
 	{
 		RestAssured.baseURI="http://localhost:3000/";
@@ -33,8 +37,65 @@ public class RAday1 {
 		given()
 			.get("ibmstudents").
 		then()
-			.statusCode(201).log().all();
+			.statusCode(200).log().all();
+		
+		given()
+		.delete("ibmstudents/3").
+		then()
+		.statusCode(200).log().all();
 	}
+	
+	@Test(enabled = false)
+	public void testcase4()
+	{
+		RestAssured.baseURI="http://localhost:3000/";
+		
+		String bodyvariable = "{\"name\":\"shobhit\",\"batchno\":\"2\"}";
+		
+		given()
+			//.contentType(ContentType.JSON)
+			//.headers("content-type","application/json")
+			.body(bodyvariable).
+		when()
+			.patch("ibmstudents").
+		then()
+			.statusCode(201)
+			.log().all();
+			
+		
+		
+	}
+	
+	@Test(enabled = true)
+	public void testcase5()
+	{
+		RestAssured.baseURI="http://localhost:3000/";
+		
+		JSONObject obj = new JSONObject();
+		//creating json body 
+		//this put is your json object class function 
+		//its not http method
+		
+		obj.put("name", "amit");
+		obj.put("batchno", 5);
+		
+		
+		
+		
+		given()
+			//.contentType(ContentType.JSON)
+			.headers("content-type","application/json")
+			.body(obj.toJSONString()).
+		when()
+			.post("ibmstudents").
+		then()
+			.statusCode(201)
+			.log().all();
+			
+		
+		
+	}
+	
 	
 	
 	
