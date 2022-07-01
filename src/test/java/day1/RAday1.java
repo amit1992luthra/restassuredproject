d989be5fd69edb70436bb5c297b2d5e797dcf1c8
amit.luthra1992@gmail.com <amit.luthra1992@gmail.com>
2022-07-01 12:23:04 +0530

    rest assured day 1

--- src/test/java/day1/RAday1.java
+++ src/test/java/day1/RAday1.java
@@ -1,6 +1,7 @@
 package day1;
 
 import org.json.simple.JSONObject;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import bsh.org.objectweb.asm.Type;
@@ -54,10 +55,10 @@ public class RAday1 {
 		
 		given()
 			//.contentType(ContentType.JSON)
-			//.headers("content-type","application/json")
+			.headers("content-type","application/json")
 			.body(bodyvariable).
 		when()
-			.patch("ibmstudents").
+			.post("ibmstudents").
 		then()
 			.statusCode(201)
 			.log().all();
@@ -66,7 +67,8 @@ public class RAday1 {
 		
 	}
 	
-	@Test(enabled = true)
+	@SuppressWarnings("unchecked")
+	@Test(enabled = false)
 	public void testcase5()
 	{
 		RestAssured.baseURI="http://localhost:3000/";
@@ -76,14 +78,67 @@ public class RAday1 {
 		//this put is your json object class function 
 		//its not http method
 		
-		obj.put("name", "amit");
-		obj.put("batchno", 5);
-		
-		
-		
+		obj.put("name", "xyz");
+		//obj.put("batchno", 2);
 		
 		given()
-			//.contentType(ContentType.JSON)
+			.headers("content-type","application/json")
+			.body(obj.toJSONString()).
+		when()
+			.patch("ibmstudents/26").
+		then()
+			.statusCode(200)
+			.log().all();
+			
+		
+		
+	}
+	
+	
+	@Test(enabled = false)
+	public void testcase6()
+	{
+		RestAssured.baseURI="https://petstore.swagger.io/v2";
+		
+	
+		given()
+		.queryParam("username", "amit")
+		.queryParam("password", "123654789").log().all().
+		when()
+			.get("/user/login").
+		then()
+			.statusCode(200)
+			.log().all();
+			
+		
+	}
+	
+	@DataProvider(name="testdata")
+	public Object[][] data()
+	{
+		Object[][] studentsdata = new Object[2][2];
+		studentsdata[0][0]= "amit";
+		studentsdata[0][1]= "5";
+		studentsdata[1][0]= "saif";
+		studentsdata[1][1]= "6";
+		return studentsdata;
+		
+	}
+	
+	@Test(enabled = true,dataProvider="testdata")
+	public void testcase7(String fname,String bno)
+	{
+		RestAssured.baseURI="http://localhost:3000/";
+		
+		JSONObject obj = new JSONObject();
+		//creating json body 
+		//this put is your json object class function 
+		//its not http method
+		
+		obj.put("name", fname);
+		obj.put("batchno", bno);
+		
+		given()
 			.headers("content-type","application/json")
 			.body(obj.toJSONString()).
 		when()
